326b53974f5aee05c18822fdb50b607d99587cca
Mohammad Javad Zare <mjz589.2018@gmail.com>
2023-06-01 00:00:30 +0330

    update v1.02

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+@admin.register(Worker)
+class WorkerAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    empty_value_display = '-'
+    list_display = ('name','phone','status', 'last_employed')
+    search_fields = ('status',)
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    empty_value_display = '-'
+    list_display = ('name','completion','budget',)
+    search_fields = ('status',)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    empty_value_display = '-'
+    list_display = ('name','completion','budget','status')
+    search_fields = ('status',)
+
+admin.site.register(ProductStatus)
--- website/models.py
+++ website/models.py
@@ -1,3 +1,60 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Worker(models.Model):
+    name = models.CharField(max_length=90)
+    phone = models.IntegerField()
+    status = models.BooleanField(default=False)
+    last_employed = models.DateField(auto_now_add=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    class Meta:
+        ordering = ('-created_date',)
+        verbose_name = ("کارگر")
+        verbose_name_plural = ("کارگران")
+    def __str__(self):
+        return "{} - {}".format(self.name, self.id)
+    
+
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    members = models.ManyToManyField(Worker)
+    budget = models.CharField(max_length=100)
+    completion = models.PositiveIntegerField(default=0)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    class Meta:
+        ordering = ('-created_date',)
+        verbose_name = ("پروژه")
+        verbose_name_plural = ("پروژه ها")
+
+    def __str__(self):
+        return "{} - {}".format(self.name, self.id)
+    
+
+class ProductStatus(models.Model):
+    name = models.CharField(max_length=100)
+    class Meta:
+        verbose_name = ("وضعیت محصول")
+        verbose_name_plural = ("وضعیت محصولات")
+    def __str__(self):
+        return "{}".format(self.name)
+    
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='product/', default='product/default.png')
+    completion = models.PositiveIntegerField(default=0)
+    status = models.ForeignKey(ProductStatus, on_delete=models.PROTECT)
+    budget = models.CharField(max_length=100)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    class Meta:
+        ordering = ('name',)
+        verbose_name = ("محصول")
+        verbose_name_plural = ("محصولات")
+
+    def __str__(self):
+        return "{} - {}".format(self.name, self.id)
+    
